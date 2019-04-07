cf2db2ec38563d3f8ee9c52c47c6e65382740644
Elan Grossman <elangrossman@gmail.com>
2019-04-07 17:04:14 -0400

    Add victims

--- UNODC_WebScrapper/casePage.py
+++ UNODC_WebScrapper/casePage.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import json
 from bs4 import BeautifulSoup as bs
 
 class CasePage:
@@ -16,7 +17,7 @@ class CasePage:
         self.html = bs(text, 'html.parser')
 
     def get_all_data(self):
-        COLUMNS = ["Title", "UNODC_NO", "Summary", "Keywords", "Procedural_Fields", "Procedural_Text"]
+        COLUMNS = ["Title", "UNODC_NO", "Summary", "Keywords", "Procedural_Fields", "Procedural_Text", "Victims"]
 
         title = self.get_title()
 
@@ -36,8 +37,9 @@ class CasePage:
         summary = self.get_summary()
         keyword_dict = self.get_keywords()
         procedural_dict, procedural_text = self.get_procedural_info()
+        victims = self.get_victims()
 
-        dataframe = pd.DataFrame([[title, unodc_no, summary, keyword_dict, procedural_dict, procedural_text]], columns=COLUMNS)
+        dataframe = pd.DataFrame([[title, unodc_no, summary, keyword_dict, procedural_dict, procedural_text, victims]], columns=COLUMNS)
 
 
         return dataframe
@@ -90,3 +92,23 @@ class CasePage:
             elif "proceduralHistoryDescription" in keyword["class"]:
                 procedure_text = " ".join([x.text for x in keyword.find_all("p")])
         return procedural_dict, procedure_text if keywords else (None, "")
+
+    def get_victims(self):
+        victim_list = []
+        victims = self.html.find("div", {"class":"victimsPlaintiffs"})
+        if not victims:
+            return None
+        for victim in victims.find_all("div", {"class":"person"}):
+            victim_dict = {}
+            for keyword in victim.find_all("div"):
+                label = keyword.find("div", {"class": "label"})
+                value = keyword.find("div", {"class": "value"})
+                # import pdb; pdb.set_trace()
+                if label:
+                    label = label.text.replace(":", "").replace("\n", "")
+                    victim_dict[label] = value.text
+
+
+            victim_list.append(victim_dict)
+
+        return victim_list
